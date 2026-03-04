826803f46f6d74d0d00d97487c838d588489ea09
MrxForte <urazimbetovazamat6@gmail.com>
2026-03-04 11:21:54 -0800

    little: Add navigation between login and sign-up screens

--- lib/common/app_strings.dart
+++ lib/common/app_strings.dart
@@ -1,14 +1,30 @@
 class AppStrings {
   AppStrings._();
 
+  // App Info
   static const String appTitle = 'CRM Pro';
+
+  // Login Screen
   static const String createAccountTitle = 'Create your account';
   static const String createAccountSubtitle =
       'To explore the world\'s exclusives';
-  static const String email = 'Email';
-  static const String password = 'Password';
-  static const String login = 'Login';
+  static const String emailLabel = 'Email';
+  static const String fullNameLabel = 'Full Name';
+  static const String fullNameHint = 'Full name';
+  static const String passwordLabel = 'Password';
+  static const String loginButton = 'Login';
+  static const String signUpButton = 'Sign Up';
   static const String alreadyHaveAccount = 'Already have an account?';
+  static const String dontYouHaveNotAccount = 'Don\'t you have an account?';
   static const String signIn = 'Sign in';
+
+  // Assets
   static const String loginImagePath = 'assets/images/image.png';
+
+  // Hints
+  static const String emailHint = 'Enter your email';
+  static const String passwordHint = 'Enter your password';
+
+  // Splash Screen
+  static const String splashImagePath = 'assets/images/image.png';
 }
